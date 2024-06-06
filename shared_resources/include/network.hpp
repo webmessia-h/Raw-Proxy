919e2cc22fddd1c50a8b2a442d7416c1ee368649
webmessia-h <iamcreepingdeath01@gmail.com>
2024-06-06 23:14:58 +0300

    basic TCP-comm interface

--- shared_resources/include/network.hpp
+++ shared_resources/include/network.hpp
@@ -1,6 +1,8 @@
 // network
 #pragma once
 #include <arpa/inet.h>
+#include <chrono> // for timeouts
+#include <cstdint>
 #include <netinet/in.h>
 #include <netinet/ip.h>  // For iphdr
 #include <netinet/tcp.h> // For tcphdr
@@ -8,80 +10,74 @@
 #include <string>
 #include <sys/types.h> // For socket types
 #include <vector>
-/* Cryptography libs*/
-#include <openssl/evp.h>
-#include <openssl/rand.h>
-#include <openssl/rsa.h>
 
-//
 namespace Network {
-#define OPT_SIZE 24
-// Calculate checksum of packet
-unsigned short checksum(void *buffer, unsigned len);
-
-// TODO: ideally implement:
+#define DATAGRAM_SIZE 1460
+#define OPT_SIZE 32
 
 /*------------------- PACKET TYPES CONSTRUCTION -----------------------*/
 // Create connection request packet
 void create_syn_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
                        unsigned char **packet, int *packet_size);
-//|---------------------------------------------------------------------|
+//----------------------------------------------------------------------|
 // Create ACK packet
 void create_ack_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
                        int32_t seq, int32_t ack_seq, unsigned char **packet,
                        int *packet_size);
-//|---------------------------------------------------------------------|
-// Read sequence
-void read_seq_and_ack(const char *packet, uint32_t *seq, uint32_t *ack);
-//|---------------------------------------------------------------------|
+//----------------------------------------------------------------------|
 // Create data packet
 void create_data_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
-                        const std::string &data, unsigned char **packet,
-                        int *packet_size);
+                        int32_t seq, int32_t ack_seq, const std::string &data,
+                        unsigned char **packet, int *packet_size);
 /*--------------------------------------------------------------------*/
 
 /*----------------  BASIC COMMUNICATEES INITIALIZATION  --------------*/
 // Create socket with some logging on exception
 int create_socket(int domain, int type, int protocol);
-//|--------------------------------------------------------------------|
+//---------------------------------------------------------------------|
 // Initialize server socket
 bool create_server_socket(int &server_sockfd, struct sockaddr_in &server_addr,
                           const char *ip, int port);
-//|--------------------------------------------------------------------|
+//---------------------------------------------------------------------|
 // Initialize client socket
 int create_client_socket(int &client_sockfd);
-//|--------------------------------------------------------------------|
+//---------------------------------------------------------------------|
 // Bind server's socket
 bool bind_to_port(int port, int &server_sockfd,
                   struct sockaddr_in &server_addr);
 /*--------------------------------------------------------------------*/
 
 /*--------------------  COMMUNICATION INTERFACE ----------------------*/
+// Read ip, tcp headers, checksum etc.
+void parse_packet(unsigned char *packet, struct sockaddr_in &client_addr,
+                  uint32_t *seq, uint32_t *ack);
+//----------------------------------------------------------------------|
+// Calculate checksum of packet
+unsigned short checksum(void *buffer, unsigned len);
+//----------------------------------------------------------------------|
 // Listen for incoming connections
-// TODO: change to receive in a loop maybe use another thread and on receival
-// notify other thread to send to server and sleep until done
-bool listen_client(int &server_sockfd, int numcl);
-//|--------------------------------------------------------------------|
+bool listen_client(int &server_sockfd, int numcl,
+                   struct sockaddr_in &server_addr,
+                   struct sockaddr_in &client_addr);
+//---------------------------------------------------------------------|
 // Make connection request
-// TODO: ideally send SYN signal and listen for SYN ACK
-bool connect_to_server(int &client_sockfd, struct sockaddr_in &server_addr,
-                       const char *ip, int port);
-//|--------------------------------------------------------------------|
+// Send SYN signal and listen for SYN ACK
+bool connect_to_server(int &client_sockfd, struct sockaddr_in &client_addr,
+                       struct sockaddr_in &server_addr, const char *ip,
+                       int port);
+//---------------------------------------------------------------------|
 // Accept pending connection request
-// TODO:: ideally respond to SYN with SYN ACK
-int accept_connection(int &server_sockfd, int &communication_sockfd);
-//|--------------------------------------------------------------------|
-// Check connection via handshake
-// TODO: encapsulate all above :)
-void handshake(int &socket_fd, sockaddr_in *dest_addr);
-//|--------------------------------------------------------------------|
+// Respond to SYN with SYN ACK
+int accept_connection(int &server_sockfd, struct sockaddr_in server_addr,
+                      struct sockaddr_in client_addr);
+//---------------------------------------------------------------------|
 // Send raw packet with some logging if exception
-ssize_t send_raw_packet(int sockfd, void *packet, size_t packet_len,
-                        struct sockaddr_in *dest_addr);
-//|--------------------------------------------------------------------|
+ssize_t send_packet(int sockfd, void *packet, size_t packet_len,
+                    struct sockaddr_in *dest_addr);
+//---------------------------------------------------------------------|
 // Receive raw packet with some logging if exception
-ssize_t receive_raw_packet(int sockfd, void *buffer, size_t buffer_len);
-//|--------------------------------------------------------------------|
+ssize_t receive_packet(int sockfd, void *buffer, size_t buffer_len);
+//---------------------------------------------------------------------|
 // Close socket
 void close_socket(int socket_fd);
 /*--------------------------------------------------------------------*/
--- shared_resources/src/network.cpp
+++ shared_resources/src/network.cpp
@@ -1,43 +1,146 @@
 #include "../include/network.hpp"
 
 #include <cerrno>
+#include <cstdint>
 #include <iostream>
+#include <netinet/in.h>
 #include <netinet/ip.h>
+#include <optional>
 #include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <thread>
 #include <unistd.h>
 
-// TODO: move to helpers
-unsigned short Network::checksum(void *buffer, unsigned len) {
-  unsigned short *buf = (unsigned short *)buffer;
-  unsigned int sum = 0;
-  unsigned short result;
+// Create connection request (SYN) packet
+void create_syn_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
+                       unsigned char **packet, int *packet_size) {
+  // TODO: check memory operations
+  size_t datagram_size =
+      sizeof(struct iphdr) + sizeof(struct tcphdr) + DATAGRAM_SIZE;
+  std::vector<unsigned char> datagram(datagram_size);
 
-  for (sum = 0; len > 1; len -= 2)
-    sum += *buf++;
+  // Ip, tcp headers
+  struct iphdr *iph = reinterpret_cast<struct iphdr *>(datagram.data());
+  struct tcphdr *tcph =
+      reinterpret_cast<struct tcphdr *>(datagram.data() + sizeof(struct iphdr));
 
-  if (len == 1)
-    sum += *(unsigned char *)buf;
+  // Ip header configuration
+  iph->ihl = 5;
+  iph->version = 4;
+  iph->tos = 0;
+  iph->tot_len = htonl(datagram_size);
+  iph->id = htonl(rand() & 65535);
+  iph->frag_off = 0;
+  iph->ttl = 64;
+  iph->protocol = IPPROTO_TCP;
+  iph->check = 0;
+  iph->saddr = src->sin_addr.s_addr;
+  iph->daddr = dst->sin_addr.s_addr;
 
-  sum = (sum >> 16) + (sum & 0xFFFF);
-  sum += (sum >> 16);
-  result = ~sum;
-  return result;
+  // TCP header configuration
+  tcph->source = src->sin_port;
+  tcph->dest = dst->sin_port;
+  tcph->seq = htonl(rand() % 4294967295);
+  tcph->ack_seq = htonl(0);
+  tcph->doff = 10; // tcp header size
+  tcph->fin = 0;
+  tcph->syn = 1;
+  tcph->rst = 0;
+  tcph->psh = 0;
+  tcph->ack = 0;
+  tcph->urg = 0;
+  tcph->check = 0;            // correct calculation follows later
+  tcph->window = htons(5840); // window size
+  tcph->urg_ptr = 0;
+
+  // TCP options are only set in the SYN packet
+  // ---- set mss ----
+  datagram[40] = 0x02;
+  datagram[41] = 0x04;
+  int16_t mss = htons(48); // mss value
+  memcpy(datagram.data() + 42, &mss, sizeof(int16_t));
+  // ---- enable SACK ----
+  datagram[44] = 0x04;
+  datagram[45] = 0x02;
+
+  // calculate checksum
+  iph->check = Network::checksum(reinterpret_cast<unsigned short *>(iph),
+                                 sizeof(struct iphdr));
+  tcph->check = Network::checksum(reinterpret_cast<unsigned short *>(tcph),
+                                  sizeof(struct tcphdr));
+  *packet = datagram.data();
+  *packet_size = iph->tot_len;
 }
 
-// Create connection request packet
-void create_syn_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
-                       unsigned char **packet, int *packet_size);
 // Create ACK packet
 void create_ack_packet(struct sockaddr_in *src, struct sockaddr_in *dst,
                        int32_t seq, int32_t ack_seq, unsigned char **packet,
-                       int *packet_size);
-// Read sequence
-void read_seq_and_ack(const char *packet, uint32_t *seq, uint32_t *ack);
+                       int *packet_size) {
+  // TODO: check memory operations
+  size_t datagram_size =
+      sizeof(struct iphdr) + sizeof(struct tcphdr) + OPT_SIZE;
+  std::vector<unsigned char> datagram(datagram_size);
 
+  // Ip, tcp headers
+  struct iphdr *iph = reinterpret_cast<struct iphdr *>(datagram.data());
+  struct tcphdr *tcph =
+      reinterpret_cast<struct tcphdr *>(datagram.data() + sizeof(struct iphdr));
+
+  // Ip header configuration
+  iph->ihl = 5;
+  iph->version = 4;
+  iph->tos = 0;
+  iph->tot_len = htonl(datagram_size);
+  iph->id = htonl(rand() & 65535);
+  iph->frag_off = 0;
+  iph->ttl = 64;
+  iph->protocol = IPPROTO_TCP;
+  iph->check = 0;
+  iph->saddr = src->sin_addr.s_addr;
+  iph->daddr = dst->sin_addr.s_addr;
+
+  // TCP header configuration
+  tcph->source = src->sin_port;
+  tcph->dest = dst->sin_port;
+  tcph->seq = htonl(rand() % 4294967295);
+  tcph->ack_seq = htonl(0);
+  tcph->doff = 10; // tcp header size
+  tcph->fin = 0;
+  tcph->syn = 0;
+  tcph->rst = 0;
+  tcph->psh = 0;
+  tcph->ack = 1;
+  tcph->urg = 0;
+  tcph->check = 0;            // correct calculation follows later
+  tcph->window = htons(5840); // window size
+  tcph->urg_ptr = 0;
+
+  // TCP options are only set in the SYN packet
+  // ---- set mss ----
+  datagram[40] = 0x02;
+  datagram[41] = 0x04;
+  int16_t mss = htons(48); // mss value
+  memcpy(datagram.data() + 42, &mss, sizeof(int16_t));
+  // ---- enable SACK ----
+  datagram[44] = 0x04;
+  datagram[45] = 0x02;
+
+  // calculate checksum
+  iph->check = Network::checksum(reinterpret_cast<unsigned short *>(iph),
+                                 sizeof(struct iphdr));
+  tcph->check = Network::checksum(reinterpret_cast<unsigned short *>(tcph),
+                                  sizeof(struct tcphdr));
+  *packet = datagram.data();
+  *packet_size = iph->tot_len;
+}
+
+// Create a data-filled packet
 void Network::create_data_packet(struct sockaddr_in *src,
-                                 struct sockaddr_in *dst,
-                                 const std::string &data,
+                                 struct sockaddr_in *dst, int32_t seq,
+                                 int32_t ack_seq, const std::string &data,
                                  unsigned char **packet, int *packet_size) {
+  // TODO: check memory operations
   size_t datagram_size =
       sizeof(struct iphdr) + sizeof(struct tcphdr) + data.size();
   std::vector<unsigned char> datagram(datagram_size);
@@ -61,15 +164,15 @@ void Network::create_data_packet(struct sockaddr_in *src,
   iph->frag_off = 0;
   iph->ttl = 64;
   iph->protocol = IPPROTO_TCP;
-  iph->check = 0; // correct calculation follows later
-  iph->saddr = src->sin_addr.s_addr;
+  iph->check = 0;                    // calculation follows later
+  iph->saddr = src->sin_addr.s_addr; // source adress
   iph->daddr = dst->sin_addr.s_addr;
 
   // TCP header configuration
   tcph->source = src->sin_port;
   tcph->dest = dst->sin_port;
-  tcph->seq = htonl(0);
-  tcph->ack_seq = htonl(0);
+  tcph->seq = htonl(seq);
+  tcph->ack_seq = htonl(ack_seq);
   tcph->doff = 10; // tcp header size
   tcph->fin = 0;
   tcph->syn = 0;
@@ -77,7 +180,7 @@ void Network::create_data_packet(struct sockaddr_in *src,
   tcph->psh = 1;
   tcph->ack = 1;
   tcph->urg = 0;
-  tcph->check = 0;            // will calculate later
+  tcph->check = 0;            // calculation follows later
   tcph->window = htons(5840); // window size
   tcph->urg_ptr = 0;
 
@@ -102,7 +205,7 @@ int Network::create_socket(int domain, int type, int protocol) {
   return sockfd;
 }
 
-// Create server socket
+// Create server socket and set address struct
 bool Network::create_server_socket(int &server_sockfd,
                                    struct sockaddr_in &server_addr,
                                    const char *ip, int port) {
@@ -110,7 +213,7 @@ bool Network::create_server_socket(int &server_sockfd,
     Network::close_socket(server_sockfd);
   }
 
-  server_sockfd = Network::create_socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+  server_sockfd = Network::create_socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
   if (server_sockfd < 0) {
     std::cerr << "Error: Failed to create server socket " << strerror(errno)
               << std::endl;
@@ -123,20 +226,20 @@ bool Network::create_server_socket(int &server_sockfd,
   return true;
 }
 
-// Create client
+// Create client and set server adress struct
 int Network::create_client_socket(int &client_sockfd) {
   if (client_sockfd != 0) {
     close_socket(client_sockfd);
   }
-  client_sockfd = create_socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
+  client_sockfd = create_socket(AF_INET, SOCK_RAW, IPPROTO_TCP);
   if (client_sockfd < 0) {
     std::cerr << "Error: Failed to create client socket " << strerror(errno)
               << std::endl;
   }
-
   return client_sockfd;
 }
 
+// bind to a port
 bool Network::bind_to_port(int port, int &server_sockfd,
                            struct sockaddr_in &server_addr) {
   server_addr.sin_port = htons(port);
@@ -153,7 +256,7 @@ bool Network::bind_to_port(int port, int &server_sockfd,
       throw std::runtime_error(std::string("Error: Failed to bind: ") +
                                strerror(errno));
     }
-    // std::cout << "Server started on port: " << port << std::endl;
+    std::cout << "Server started on port: " << port << std::endl;
     return true;
   } catch (const std::exception &ex) {
     std::cerr << ex.what() << std::endl;
@@ -162,29 +265,79 @@ bool Network::bind_to_port(int port, int &server_sockfd,
   }
 }
 
-// Listen for incoming connections
-bool Network::listen_client(int &server_sockfd, int numcl) {
-  // FIXME: there is no 'listen' on SOCK_RAW, implement another method
-  // just `recvfrom()` in a infinite loop
-  if (listen(server_sockfd, numcl) < 0) {
-    std::cerr << "Error: Failed to listen " << strerror(errno) << std::endl;
-    Network::close_socket(server_sockfd);
-    return false;
+void parse_packet(unsigned char *packet, uint32_t *seq, uint32_t *ack,
+                  std::optional<struct sockaddr_in> &source) {
+  struct iphdr *iph = reinterpret_cast<struct iphdr *>(packet);
+  unsigned short iphdrlen = iph->ihl * 4;
+
+  struct tcphdr *tcph = reinterpret_cast<struct tcphdr *>(packet + iphdrlen);
+  if (source) {
+    // read source ip address
+    source->sin_addr.s_addr = iph->saddr;
+    // read source port
+    source->sin_port = tcph->source;
   }
-  // std::cout << "Listening for incoming connection" << std::endl;
-  return true;
+  // read sequence number
+  uint32_t seq_num = tcph->seq;
+  // read acknowledgement number
+  uint32_t ack_num = tcph->ack_seq;
+  *seq = ntohl(seq_num);
+  *ack = ntohl(ack_num);
+  printf("sequence number: %lu\n", (unsigned long)*seq);
+  printf("acknowledgement number: %lu\n", (unsigned long)*seq);
 }
 
-void Network::handshake(int &socket_fd, sockaddr_in *dest_addr) {
-  /*FIXME:  just encapsulate everything we need for the handshake
-    std::cout.write(msg, strlen(msg));
-    std::cout << "\n";
-    return;
-  */
+unsigned short Network::checksum(void *buffer, unsigned len) {
+  unsigned short *buf = (unsigned short *)buffer;
+  unsigned int sum = 0;
+  unsigned short result;
+
+  for (sum = 0; len > 1; len -= 2)
+    sum += *buf++;
+
+  if (len == 1)
+    sum += *(unsigned char *)buf;
+
+  sum = (sum >> 16) + (sum & 0xFFFF);
+  sum += (sum >> 16);
+  result = ~sum;
+  return result;
+}
+
+// Listen for incoming connections
+bool Network::listen_client(int &server_sockfd, int numcl,
+                            struct sockaddr_in &server_addr,
+                            struct sockaddr_in &client_addr) {
+  std::cout << "Listening for incoming connection..." << std::endl;
+  unsigned char syn_req[DATAGRAM_SIZE];
+  uint32_t seq_num, ack_num;
+  // Define the duration for which the loop should run
+  auto duration = std::chrono::minutes(5);
+  // Get the start time
+  auto start_time = std::chrono::steady_clock::now();
+  // Calculate the end time
+  auto end_time = start_time + duration;
+  ssize_t bytes_recv = 0;
+  while (std::chrono::steady_clock::now() < end_time) {
+    bytes_recv += receive_packet(server_sockfd, &syn_req, DATAGRAM_SIZE);
+    if (bytes_recv > 0)
+      break;
+    std::this_thread::sleep_for(std::chrono::seconds(1));
+  }
+  // Read ip,tcp headers to acknowledge client
+  if (bytes_recv > 0) {
+    std::cout << "Received SYN" << std::endl;
+    Network::parse_packet(syn_req, client_addr, &seq_num, &ack_num);
+    return true;
+  } else {
+    std::cout << "Client connection timeout" << std::endl;
+    return false;
+  }
 }
 
 // Make connection request
 bool Network::connect_to_server(int &client_sockfd,
+                                struct sockaddr_in &client_addr,
                                 struct sockaddr_in &server_addr, const char *ip,
                                 int port) {
   //  Get server's ip and port number
@@ -192,39 +345,47 @@ bool Network::connect_to_server(int &client_sockfd,
   server_addr.sin_port = htons(port);
   server_addr.sin_addr.s_addr = inet_addr(ip);
   // Try to connect to server
-  // FIXME: there is no `connect()` just fill the server addr and perform
-  // handshake
-  if (connect(client_sockfd, reinterpret_cast<sockaddr *>(&server_addr),
-              sizeof(server_addr)) < 0) {
-    // Failed to connect
-    std::cerr << "Error: Client failed to connect to:"
-              << inet_ntoa(server_addr.sin_addr) << " on port: " << port << "\t"
-              << strerror(errno) << std::endl;
-    close_socket(client_sockfd);
-    return false;
+  // send SYN until received SYN-ACK or TIMEOUT
+  int wait_duration = 5; // in seconds
+  unsigned char *packet;
+  unsigned char response[DATAGRAM_SIZE];
+  int packet_size;
+  uint32_t seq_num, ack_num;
+  Network::create_syn_packet(&client_addr, &server_addr, &packet, &packet_size);
+  while (true) {
+    std::this_thread::sleep_for(std::chrono::seconds(wait_duration));
+    Network::send_packet(client_sockfd, packet, packet_size, &server_addr);
+    ssize_t bytes_recv =
+        Network::receive_packet(client_sockfd, response, sizeof(response));
+    wait_duration += 5;
+    if (wait_duration == 120) {
+      std::cout << "Connection timeout" << std::endl;
+      return false;
+    }
+    if (bytes_recv > 0)
+      break;
+    Network::parse_packet(response, server_addr, &seq_num, &ack_num);
   }
+
   return true;
 }
 
 // Accept incoming connections
-int Network::accept_connection(int &server_sockfd, int &communication_sockfd) {
-  // Create communication socket
-  struct sockaddr_in client_addr;
-  socklen_t client_addr_len = sizeof(client_addr);
-  // FIXME: there is no `accept()` just send SYN ACK
-  communication_sockfd =
-      accept(server_sockfd, reinterpret_cast<struct sockaddr *>(&client_addr),
-             &client_addr_len);
-  if (communication_sockfd < 0) {
-    std::cerr << "Error: Connection was not accepted " << strerror(errno)
-              << std::endl;
-    return -1;
-  }
-  return communication_sockfd;
+int Network::accept_connection(int &server_sockfd,
+                               struct sockaddr_in server_addr,
+                               struct sockaddr_in client_addr) {
+  // send ACK packet
+  unsigned char *packet;
+  int packet_size;
+  Network::create_ack_packet(&server_addr, &client_addr, 0, 1, &packet,
+                             &packet_size);
+  Network::send_packet(server_sockfd, packet, packet_size, &client_addr);
+  std::cout << "Sent SYN-ACK" << std::endl;
+  return 0;
 }
 
-ssize_t Network::send_raw_packet(int sockfd, void *packet, size_t packet_len,
-                                 struct sockaddr_in *dest_addr) {
+ssize_t Network::send_packet(int sockfd, void *packet, size_t packet_len,
+                             struct sockaddr_in *dest_addr) {
   ssize_t bytes_sent =
       sendto(sockfd, packet, packet_len, 0, (struct sockaddr *)dest_addr,
              sizeof(struct sockaddr_in));
@@ -234,8 +395,7 @@ ssize_t Network::send_raw_packet(int sockfd, void *packet, size_t packet_len,
   return bytes_sent;
 }
 
-ssize_t Network::receive_raw_packet(int sockfd, void *buffer,
-                                    size_t buffer_len) {
+ssize_t Network::receive_packet(int sockfd, void *buffer, size_t buffer_len) {
   struct sockaddr_in src_addr;
   socklen_t addr_len = sizeof(src_addr);
   ssize_t bytes_received = recvfrom(sockfd, buffer, buffer_len, 0,
